4c622ce9f7fd2ae1de056415e1691e0567c2dc04
Override <112493528+0V3RR1DE0@users.noreply.github.com>
2024-05-15 18:31:40 +0300

    Update gamejs.js

--- static/scripts/gamejs.js
+++ static/scripts/gamejs.js
@@ -1,4 +1,7 @@
-document.addEventListener('DOMContentLoaded', async (event) => {
+import { LoadingBar } from './loadingbar.js';
+import { randomEncounter } from './encounters.js';
+
+document.addEventListener('DOMContentLoaded', (event) => {
     const inventoryButton = document.getElementById('inventory-button');
     const inventoryModal = document.getElementById('inventory-modal');
     const closeBtn = document.querySelector('.close');
@@ -12,52 +15,8 @@ document.addEventListener('DOMContentLoaded', async (event) => {
     const gameContainer = document.getElementById('game-container');
     const actionMessageBox = document.getElementById('action-message-box');
 
-    // Load item and entity data from JSON files
-    const items = await fetch('static/assets/items/stone_sword.json').then(response => response.json());
-    const entities = await fetch('static/assets/entities/skeleton.json').then(response => response.json());
-
-    // Simulate loading progress and random messages
-    let startTime = Date.now();
-    let currentMessageIndex = 0;
-    let progress = 0;
-    const messages = [
-        'Building Terrain',
-        'Generating Rocks',
-        'Loading Assets'
-    ];
-    
-    // Define durations for each message
-    const durations = [3000, 7000, 5000]; // durations in milliseconds
-    
-    function getRandomMessage() {
-        return messages[Math.floor(Math.random() * messages.length)];
-    }
-    
-    const loadingInterval = setInterval(() => {
-        const elapsed = Date.now() - startTime;
-    
-        // Calculate progress based on time elapsed
-        progress = Math.min(100, (elapsed / durations[currentMessageIndex]) * 100);
-    
-        loadingProgress.style.width = progress + '%';
-    
-        // Display the current message with the calculated progress
-        loadingMessage.textContent = `${messages[currentMessageIndex]} ${Math.floor(progress)}%`;
-    
-        if (progress >= 100) {
-            // If progress reaches 100%, move to the next message
-            currentMessageIndex++;
-    
-            // If all messages have been displayed, reset the currentMessageIndex to 0
-            if (currentMessageIndex >= messages.length) {
-                currentMessageIndex = 0;
-            }
-    
-            // Reset start time for the next message
-            startTime = Date.now();
-            progress = 0;
-        }
-    }, 100);
+    const loadingBar = new LoadingBar(loadingProgress, loadingMessage);
+    loadingBar.start();
 
     // Show inventory modal
     inventoryButton.addEventListener('click', () => {
@@ -110,18 +69,7 @@ document.addEventListener('DOMContentLoaded', async (event) => {
             if (wasInventoryOpen) {
                 inventoryModal.style.display = 'block';
             }
-            // Random chance to encounter NPC or monster (20% chance)
-            const encounterChance = Math.random();
-            if (encounterChance <= 0.2) {
-                // Select a random character
-                const character = entities[Math.floor(Math.random() * entities.length)];
-                // Prompt the player with a confirmation box
-                const approachConfirmation = confirm(`You encountered a ${character.entity}. Do you want to approach?`);
-                if (approachConfirmation) {
-                    // If player chooses to approach, redirect to character's page
-                    window.location.href = character.page;
-                }
-            }
+            randomEncounter();
         }, 5000);
     }
 });
