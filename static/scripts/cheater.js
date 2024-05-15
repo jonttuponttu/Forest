ff1b0b8e15bc53a68bef7bd82ab261943d3b3d23
Override <112493528+0V3RR1DE0@users.noreply.github.com>
2024-05-15 19:51:15 +0300

    Update cheater.js

--- static/scripts/cheater.js
+++ static/scripts/cheater.js
@@ -1,11 +1,11 @@
 // cheater.js
 window.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const cheated = urlParams.get('cheated');
+    const cheated = localStorage.getItem('cheated');
 
     if (cheated) {
         setTimeout(() => {
             alert('Loadingbar Skipped!\nCheater!');
+            localStorage.removeItem('cheated'); // Remove the cheat status from the local storage
         }, 1000); // Delay of 1 second
     }
 });
