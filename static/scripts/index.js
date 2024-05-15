17f848f39cfbe1f862b4c903966c17d650524d9e
Override <112493528+0V3RR1DE0@users.noreply.github.com>
2024-05-15 20:03:25 +0300

    Update index.js

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,3 +1,6 @@
 document.getElementById("play-button").addEventListener("click", function() {
+    // Set cheats to 0
+    localStorage.setItem('cheats', '0');
+
     window.location.href = "loadingbar.html";
 });
